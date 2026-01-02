12eb73b4af1d5b6b8999ce9f44ed4cedfa3e77ea
Edson Fell <edsonfell22@gmail.com>
2026-01-02 13:15:12 -0300

    added ssl condition to postgresql connection when environment is prod

--- infra/database.js
+++ infra/database.js
@@ -1,27 +1,26 @@
-import { Client, Result } from 'pg'
+import { Client, Result } from "pg";
 
 async function query(queryObject) {
-    
-    const client = new Client({
-        database: process.env.POSTGRES_DB,
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD
-    })
-    await client.connect()
-    try {
-        const result = await client.query(queryObject)
-        return result;
-
-    } catch(error) {
-        console.log(error)
-        
-    } finally {
-        await client.end()
-    }
+  const client = new Client({
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV == "development" ? false : true,
+  });
+  try {
+    await client.connect();
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
-    query: query
-}
+  query: query,
+};
